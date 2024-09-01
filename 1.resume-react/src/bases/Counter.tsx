80b3ee223ee43bfbf719460759538658d1d01dfd
Carlos Orrego <carlosorrego5151@gmail.com>
2024-09-01 12:16:28 -0500

    feat: add actions counter and styles

--- 1.resume-react/src/bases/Counter.tsx
+++ 1.resume-react/src/bases/Counter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface Props {
   initialState?: number;
@@ -19,6 +19,11 @@ const Counter: React.FC<Props> = ({ initialState }) => {
     value: initialState ?? 0,
     clicks: 0,
   });
+  const [showDetails, setShowDetails] = useState(false);
+  const [length, setLength] = useState(true);
+
+  const numberForm = useRef<HTMLInputElement>(null);
+  const operation = useRef<HTMLSelectElement>(null);
 
   const onClickCounter = (operator: Operations, value = 1) => {
     if (counter.value <= 0 && operator === Operations.REST) return;
@@ -37,23 +42,71 @@ const Counter: React.FC<Props> = ({ initialState }) => {
 
   return (
     <div className="counter">
-      <h1 className="title">Counter {counter.value}</h1>
-      <h1 className="touch">Touched {counter.clicks}</h1>
-      <button
-        onClick={() => onClickCounter(Operations.REST)}
-        className="rest-button"
+      <div className="operation">
+        <button
+          onClick={() => onClickCounter(Operations.REST)}
+          className="button-oper rest-button"
+        >
+          -
+        </button>
+        <span className="counter-value">
+          {counter.value}
+          <span className="touch" title="touched">
+            {counter.clicks}
+          </span>
+        </span>
+        <button
+          onClick={() => onClickCounter(Operations.SUM)}
+          className="button-oper plus-button"
+        >
+          +
+        </button>
+      </div>
+      <div className="button-details">
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "Ocultar detaller" : "Mostrar detalles"}
+        </button>
+      </div>
+      <form
+        className={`${!showDetails ? "disabled" : ""} form`}
+        onSubmit={(e) => {
+          e.preventDefault();
+          const number = numberForm.current?.value;
+          const operator = operation.current?.value ?? Operations.SUM;
+          onClickCounter(Number(operator), Number(number));
+        }}
       >
-        -
-      </button>
-      <button
-        onClick={() => onClickCounter(Operations.SUM)}
-        className="plus-button"
-      >
-        +
-      </button>
-      <button onClick={() => onClickCounter(Operations.SUM, 5)}>
-        plus 5 points
-      </button>
+        <div className="input">
+          <label htmlFor="number">Number</label>
+          <input
+            type="number"
+            name="number"
+            id="number"
+            min={1}
+            ref={numberForm}
+            onChange={(e) => {
+              if (e.target.value.length === 0) {
+                setLength(true);
+                return;
+              }
+              setLength(false);
+            }}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="operations">Operaciones</label>
+          <select
+            name="operations"
+            id="operations"
+            defaultValue={Operations.SUM}
+            ref={operation}
+          >
+            <option value={Operations.REST}>Rest</option>
+            <option value={Operations.SUM}>Plus</option>
+          </select>
+        </div>
+        <input type="submit" value="Operar" disabled={length} />
+      </form>
     </div>
   );
 };
