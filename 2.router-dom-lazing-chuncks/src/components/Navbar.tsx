56cb0c2c4ed9d744f32d806523e100e48fa42ce5
Carlos Orrego <carlosorrego5151@gmail.com>
2024-09-03 10:51:30 -0500

    refactor: add resposive design navbar and content

--- 2.router-dom-lazing-chuncks/src/components/Navbar.tsx
+++ 2.router-dom-lazing-chuncks/src/components/Navbar.tsx
@@ -1,12 +1,32 @@
 import { NavLink, Outlet } from "react-router-dom";
 import Logo from "../../public/vite.svg";
 import { ROUTES } from "../utils/routes";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const handlerMenu = () => setShowMenu(!showMenu);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 800) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="container">
       <header>
-        <nav>
+        <nav className={`${showMenu ? "visible-menu" : ""}`}>
           <div className="logo">
             <img src={Logo} alt="logo navbar" />
           </div>
@@ -16,18 +36,27 @@ const Navbar = () => {
                 <NavLink
                   to={href}
                   className={({ isActive }) => (isActive ? "active" : "")}
+                  onClick={handlerMenu}
                 >
                   {appearance}
                 </NavLink>
               </li>
             ))}
           </ul>
+          {showMenu && (
+            <span className="icon icon-x" onClick={handlerMenu}>
+              <i className="fa-solid fa-x fa-xl"></i>
+            </span>
+          )}
         </nav>
       </header>
       <main>
-        <div className="content">
-          <Outlet />
-        </div>
+        {!showMenu && (
+          <span className="icon icon-bars" onClick={handlerMenu}>
+            <i className="fa-solid fa-bars fa-xl"></i>
+          </span>
+        )}
+        <Outlet />
       </main>
     </div>
   );
