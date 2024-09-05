d25a764ab884641eda6b04961af51ab7ba84bf87
Carlos Orrego <carlosorrego5151@gmail.com>
2024-09-05 17:03:19 -0500

    refactor: implement pattern Component Compount by individual element

--- 3.component-compound/src/components/ProductCard.tsx
+++ 3.component-compound/src/components/ProductCard.tsx
@@ -1,11 +1,10 @@
 import ProductContextProvider from "../context/productContext";
 import useProduct from "../hooks/useProduct";
-import AddCartButton from "./partials/AddCartButton";
-import Counter from "./partials/Counter";
-import Details from "./partials/Details";
-import Image from "./partials/Image";
 
-const ProductCard: React.FC<{ id: string }> = ({ id }) => {
+const ProductCard: React.FC<{ children: React.ReactNode[]; id: string }> = ({
+  id,
+  children,
+}) => {
   const { state, increase, decrease, addFav, addCart } = useProduct({ id });
 
   const { product } = state;
@@ -14,12 +13,7 @@ const ProductCard: React.FC<{ id: string }> = ({ id }) => {
     <ProductContextProvider
       value={{ product, redux: { state, increase, decrease, addCart, addFav } }}
     >
-      <div className="card">
-        <Image />
-        <Details />
-        <Counter />
-        <AddCartButton />
-      </div>
+      <div className="card">{children}</div>
     </ProductContextProvider>
   );
 };
--- 3.component-compound/src/page/ShoppingCart.tsx
+++ 3.component-compound/src/page/ShoppingCart.tsx
@@ -1,3 +1,7 @@
+import AddCartButton from "../components/partials/AddCartButton";
+import Counter from "../components/partials/Counter";
+import Details from "../components/partials/Details";
+import Image from "../components/partials/Image";
 import ProductCard from "../components/ProductCard";
 import { PRODUCTS } from "../utils/product";
 
@@ -6,9 +10,15 @@ const ShoppingCart = () => {
     <div>
       <h1>Produts</h1>
       <hr />
+      {/* Component Compound By Single Elements */}
       <div className="products">
         {PRODUCTS.map((product) => (
-          <ProductCard id={product.id} key={product.id} />
+          <ProductCard id={product.id} key={product.id}>
+            <Image />
+            <Details />
+            <Counter />
+            <AddCartButton />
+          </ProductCard>
         ))}
       </div>
     </div>
