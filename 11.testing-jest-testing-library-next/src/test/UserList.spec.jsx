80613bec8abb3fe4208e08f6261be7d82ace311b
Carlos Orrego <carlosorrego5151@gmail.com>
2024-09-23 09:57:55 -0500

    feat: add modification req server

--- 11.testing-jest-testing-library-next/src/test/UserList.spec.jsx
+++ 11.testing-jest-testing-library-next/src/test/UserList.spec.jsx
@@ -1,10 +1,13 @@
 import Users from "@/app/users/page";
 import { act, render, screen, waitFor } from "@testing-library/react";
+import { server } from "./mocks/server";
+import { rest } from "msw";
 
 describe("Test for page users", () => {
   describe("Rendering", () => {
     it("should render the page", async () => {
       render(<Users />);
+      expect(screen.getByText(/Wait for user/)).toBeInTheDocument();
       expect(
         await screen.findByText(
           "carlos",
@@ -14,6 +17,17 @@ describe("Test for page users", () => {
           }
         )
       ).toBeInTheDocument();
+      expect(screen.queryByText(/Wait for user/)).not.toBeInTheDocument();
+    });
+
+    it("Should be render name Mike", async () => {
+      server.use(
+        rest.get("/api/users", (_, res, ctx) => {
+          return res(ctx.json([{ id: 1, userName: "Mike" }]));
+        })
+      );
+      render(<Users />);
+      expect(await screen.findByText("Mike")).toBeInTheDocument();
     });
   });
 });
