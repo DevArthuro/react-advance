a95d73f55bb16585f753e79e88b150deed9cefa4
Carlos Orrego <carlosorrego5151@gmail.com>
2024-09-23 15:21:50 -0500

    feat: add base structure 9.3 example

---
+++ 9.3.desing-patterns-frontend/src/app/_components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Navbar: React.FC<Props> = ({ children }) => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/HOC">Hight Order Component (HOC)</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  );
+};
+
+export default Navbar;
