fade8135a3819b44f8c101dd340462963a3403c5
carlos orrego <carlosorrego5151@gmail.com>
2025-01-29 23:36:33 -0500

    style: add styles form

--- 9.7.forms-formik-and-test/src/components/FormLogin.tsx
+++ 9.7.forms-formik-and-test/src/components/FormLogin.tsx
@@ -15,8 +15,16 @@ const FormLogin = () => {
           <Image src={ImageForm} className="form__image-img" alt="logo image" />
         </div>
         <div className="form__inputs">
-          <input type="text" className="form__inputs-username" />
-          <input type="password" className="form__inputs-password" />
+          <input
+            type="text"
+            className="form__inputs-username"
+            placeholder="username"
+          />
+          <input
+            type="password"
+            className="form__inputs-password"
+            placeholder="password"
+          />
         </div>
         <div className="form__button">
           <button>Login</button>
